a9533d15cb4ab7498e118794876f35e747eb74f7
xiuxiu628 <lWenjie@mail.ustc.edu.cn>
2021-09-14 15:39:28 +0800

    9-14

--- src/views/login.vue
+++ src/views/login.vue
@@ -10,35 +10,58 @@
     <!-- /导航栏 -->
 
     <!-- 登录表单 -->
-    <van-cell-group>
+    <van-form
+      :show-error="false"
+      :show-error-message="false"
+      validate-first
+      ref="login-form"
+      @submit="onLogin"
+      @failed="onFailed"
+    >
       <van-field
         v-model="user.mobile"
+        clearable
+        name="mobile"
         left-icon="phone-circle-o"
         placeholder="请输入手机号"
+        :rules="formRules.mobile"
       />
       <van-field
         v-model="user.code"
         clearable
+        name="code"
         left-icon="info-o"
         placeholder="请输入验证码"
+        :rules="formRules.code"
       >
         <template #button>
+          <van-count-down
+            v-if="isCountDownShow"
+            :time="1000 * 60"
+            format="ss s"
+            @finish="isCountDownShow = false"
+          />
           <van-button
+            v-else
             class="send-btn"
             size="mini"
             round
-          >发送验证码</van-button>
+            :loading="isSendSmsLoading"
+            @click.prevent="onSendSms"
+            >发送验证码</van-button
+          >
         </template>
       </van-field>
-    </van-cell-group>
-    <div class="login-btn-wrap">
-      <van-button class="login-btn" type="info" block>登录</van-button>
-    </div>
-    <!-- /登录表单 -->
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" type="info" block>登录</van-button>
+      </div>
+    </van-form>
   </div>
 </template>
 
 <script>
+import { sendSms, login } from '@/api/user'
+
 export default {
   name: 'LoginIndex',
   components: {},
@@ -48,6 +71,18 @@ export default {
       user: {
         mobile: '', // 手机号
         code: '' // 验证码
+      },
+      isSendSmsLoading: false,
+      isCountDownShow: false,
+      formRules: {
+        mobile: [
+          { required: true, message: '请输入手机号' },
+          { pattern: /^1[3|5|7|8|9]\d{9}$/, message: '手机号格式错误' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '验证码格式错误' }
+        ]
       }
     }
   },
@@ -55,7 +90,68 @@ export default {
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    async onLogin () {
+      // 开始转圈圈
+      this.$toast.loading({
+        duration: 0, // 持续时间，0表示持续展示不停止
+        forbidClick: true, // 是否禁止背景点击
+        message: '登录中...' // 提示消息
+      })
+      try {
+        const res = await login(this.user)
+        console.log('登录成功', res)
+        // 提示 success 或者 fail 的时候，会先把其它的 toast 先清除
+        this.$toast.success('登录成功')
+      } catch (err) {
+        console.log('登录失败', err)
+        this.$toast.fail('登录失败，手机号或验证码错误')
+      }
+    },
+    onFailed (error) {
+      if (error.errors[0]) {
+        this.$toast({
+          message: error.errors[0].message, // 提示消息
+          position: 'top' // 防止手机键盘太高看不见提示消息
+        })
+      }
+    },
+    async onSendSms () {
+      try {
+        // 校验手机号码
+        await this.$refs['login-form'].validate('mobile')
+        // 验证通过，请求发送验证码
+        this.isSendSmsLoading = true
+        await sendSms(this.user.mobile)
+        this.isCountDownShow = true
+      } catch (err) {
+        // try 里面任何代码的错误都会进入 catch
+        // 不同的错误需要有不同的提示，那就需要判断了
+        let message = ''
+        if (err && err.response && err.response.status === 429) {
+          // 发送短信失败的错误提示
+          message = '发送太频繁了，请稍后重试'
+        } else if (err.name === 'mobile') {
+          // 表单验证失败的错误提示
+          message = err.message
+        } else {
+          // 未知错误
+          message = '发送失败，请稍后重试'
+        }
+
+        // 提示用户
+        this.$toast({
+          message,
+          position: 'top'
+        })
+      }
+      // 无论发送验证码成功还是失败，最后都要关闭发送按钮的 loading 状态
+      this.isSendSmsLoading = false
+
+      // 发送失败，显示发送按钮，关闭倒计时
+      this.isCountDownShow = false
+    }
+  }
 }
 </script>
 
